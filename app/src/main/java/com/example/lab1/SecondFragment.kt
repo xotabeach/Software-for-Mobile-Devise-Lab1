aa9a67923838ca8673c810986f20bfaa774545ce
xotabeach <xotabich1941@gmail.com>
2023-10-02 17:34:16 +0300

    there 1st try to create git branch :)

---
+++ app/src/main/java/com/example/lab1/SecondFragment.kt
@@ -0,0 +1,54 @@
+package com.example.lab1
+import android.widget.TextView
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.lab1.databinding.FragmentSecondBinding
+
+
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        val firstNameTextView = view.findViewById<TextView>(R.id.firstNameTextView)
+        val lastNameTextView = view.findViewById<TextView>(R.id.lastNameTextView)
+
+
+        val firstName = "Никита"
+        val lastName = "Тарасов"
+
+        firstNameTextView.text = firstName
+        lastNameTextView.text = lastName
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
